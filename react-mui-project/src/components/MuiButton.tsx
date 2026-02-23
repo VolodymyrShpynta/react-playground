17126d2fe88e8c962a534800c3ee7399d142b74e
vshpynta <vladimir.shpinta@gmail.com>
2026-02-23 13:19:51 +0100

    Remove unneccessary import. Add MuiButton examples.

---
+++ react-mui-project/src/components/MuiButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@mui/material'
+
+export const MuiButton = () => {
+  return <>
+    <Button variant="contained">contained</Button>
+    <Button variant="text">Text</Button>
+    <Button variant="outlined">Outlined</Button>
+  </>
+}
