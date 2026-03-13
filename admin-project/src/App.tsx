a129c9244486e6b0267b062ef9318cb8cd4db73d
vshpynta <vladimir.shpinta@gmail.com>
2026-03-13 22:49:38 +0100

    [admin-project] Fix light-dark mode support. Add top bar.

--- admin-project/src/App.tsx
+++ admin-project/src/App.tsx
@@ -1,5 +1,6 @@
-import { ThemeProvider } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import { ColorModeToggleContext, useColorTheme } from "./theme"
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
   const [theme, colorModeToggle] = useColorTheme();
@@ -7,12 +8,15 @@ function App() {
   return (
     <ColorModeToggleContext.Provider value={colorModeToggle}>
       <ThemeProvider theme={theme}>
+        <CssBaseline /> {/* Applies theme-aware global reset so body/background follow light-dark mode  */}
         <div className="app">
-          <main className="content">Admin Dashboard</main>
+          <main className="content">
+            <Topbar />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeToggleContext.Provider>
   )
 }
 
-export default App
+export default App;
--- admin-project/src/scenes/global/Topbar.tsx
+++ admin-project/src/scenes/global/Topbar.tsx
@@ -1,8 +1,51 @@
+import { useContext } from "react";
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import { colorTokens, ColorModeToggleContext } from "../../theme";
+import SearchIcon from '@mui/icons-material/Search';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PersonIcon from '@mui/icons-material/Person';
+
 const Topbar = () => {
+  const theme = useTheme();
+  const colors = colorTokens(theme.palette.mode);
+  const colorModeToggle = useContext(ColorModeToggleContext);
+
   return (
-    <div>
-      Topbar
-    </div>
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* Search bar */}
+      <Box
+        display="flex"
+        sx={{
+          backgroundColor: colors.primary[400],
+          borderRadius: "3px"
+        }}>
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* Icons */}
+      <Box display="flex">
+        <IconButton onClick={colorModeToggle.toggleColorMode}>
+          {theme.palette.mode === "dark"
+            ? (<DarkModeIcon />)
+            : (<LightModeIcon />)}
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+        <IconButton>
+          <PersonIcon />
+        </IconButton>
+      </Box>
+    </Box>
   )
 }
 
